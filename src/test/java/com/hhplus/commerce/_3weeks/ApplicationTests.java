a91f8cb8565fbff59ed59f6ed718721314718de1
jogwangho0619@gmail.com <jogwangho0619@gmail.com>
2024-10-10 01:18:05 +0900

    init

---
+++ src/test/java/com/hhplus/commerce/_3weeks/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.commerce._3weeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
