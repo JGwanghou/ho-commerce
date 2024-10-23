f4942f8ba143bbc73e763dcb3cf6f0ec8b3346d9
jogwangho0619@gmail.com <jogwangho0619@gmail.com>
2024-10-23 22:10:47 +0900

    BaseEntity 충돌 수정

--- src/main/java/com/hhplus/commerce/_3weeks/common/config/BaseEntity.java
+++ src/main/java/com/hhplus/commerce/_3weeks/common/config/BaseEntity.java
@@ -1,11 +1,8 @@
 package com.hhplus.commerce._3weeks.common.config;
 
-import com.hhplus.commerce._3weeks.infra.order.OrderEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -15,10 +12,12 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime create_at;
 
     @LastModifiedDate
     private LocalDateTime update_at;
+
 }
