c930fbcea5dd21779da9b83813ae0258dbaed1f6
JGwanghou <jogwangho0619@gmail.com>
2024-11-07 18:16:22 +0900

    fix: 기타 파일 수정

--- src/test/java/com/hhplus/commerce/_3weeks/domain/product/ProductServiceTest.java
+++ src/test/java/com/hhplus/commerce/_3weeks/domain/product/ProductServiceTest.java
@@ -35,8 +35,8 @@ class ProductServiceTest {
         List<Product> products = productService.readProductAll();
 
         assertEquals(2, products.size());
-        assertEquals("초코파이", products.get(0).getName());
-        assertEquals("새송이버섯", products.get(1).getName());
+        assertEquals("초코파이", products.get(0).getProduct_name());
+        assertEquals("새송이버섯", products.get(1).getProduct_name());
     }
 
     @Test
@@ -51,8 +51,8 @@ class ProductServiceTest {
         List<Product> products = productService.readProductByIds(productIds);
 
         assertEquals(2, products.size());
-        assertEquals("초코파이", products.get(0).getName());
-        assertEquals("새송이버섯", products.get(1).getName());
+        assertEquals("초코파이", products.get(0).getProduct_name());
+        assertEquals("새송이버섯", products.get(1).getProduct_name());
     }
 
     @Test
@@ -88,7 +88,7 @@ class ProductServiceTest {
         Product result = productService.readProductDetail(productId);
 
         assertEquals(1L, result.getId());
-        assertEquals("초코파이", result.getName());
+        assertEquals("초코파이", result.getProduct_name());
         assertEquals(2000, result.getPrice());
         assertEquals(10, result.getStock());
     }
@@ -107,10 +107,10 @@ class ProductServiceTest {
         List<Product> populars = productService.readProductPopulars();
 
         assertEquals(2, populars.size());
-        assertEquals(populars.get(0).getName(), products.get(0).getName());
+        assertEquals(populars.get(0).getProduct_name(), products.get(0).getProduct_name());
         assertEquals(populars.get(0).getSale_count(), products.get(0).getSale_count());
 
-        assertEquals(populars.get(1).getName(), products.get(1).getName());
+        assertEquals(populars.get(1).getProduct_name(), products.get(1).getProduct_name());
         assertEquals(populars.get(1).getSale_count(), products.get(1).getSale_count());
     }
 }
